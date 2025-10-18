f8f079b12ad00b1eaa18dc20c876f67fc30d6d96
RRRwwe <rn8530638@gmail.com>
2025-10-18 12:28:33 +0300

    Leson 18

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import * as trpcExpress from "@trpc/server/adapters/express"
+import cors from 'cors'
 import express from "express"
 import { trpcRouter } from "./trpc"
-import cors from "cors"
+
 
 const expressApp = express()
 expressApp.use(cors())
--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -10,6 +10,7 @@ const ideas = [
 
 const trpc = initTRPC.create()
 
+
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas }
